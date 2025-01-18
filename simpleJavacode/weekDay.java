99dee15ce9719ef407b5d859a8533eb3545292b0
Vivek Raj Sahay <vivekaman742@gmail.com>
2025-01-18 19:10:37 +0530

    Create weekDay.java

---
+++ simpleJavacode/weekDay.java
@@ -0,0 +1,30 @@
+
+public class week {
+    public static void main (String a[])
+    {
+        int n=5;
+        switch (n)
+        {
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thrusday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            case 7:
+            System.out.println("Sunday");
+            break;
+        }
+    }
+}
