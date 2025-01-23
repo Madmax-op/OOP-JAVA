cb466aaf4ed54522fae5c0d225b9a16dbfefddc1
Vivek Raj Sahay <vivekaman742@gmail.com>
2025-01-23 21:45:37 +0530

    Create largestNumber.java

---
+++ array|commandLine/largestNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class LargestNumber {
+    public static void main(String a[]	) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three numbers:");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        int largest = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+        System.out.println("The largest number is: " + largest);
+    }
+}
